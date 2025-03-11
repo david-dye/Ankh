da467f1cbfe3f30c72a34417498707de6a9d0ed4
David Dye <ddye@college.harvard.edu>
2025-03-10 23:39:20 -0400

    Added an extremely broken parser to the lexer code. TODO: (1) Split code into main.cpp, lexer.cpp, and parser.cpp, with appropriate header files. (2) Edit parser to handle errors correctly. (3) Edit parser to handle types correctly. (4) Add types to parser
    All of the above must be done before we can get usable LLVM bytecode.

--- Ankh/lexer.cpp
+++ Ankh/lexer.cpp
@@ -4,6 +4,8 @@
 #include <string>
 #include <iostream>
 #include <cstdio>
+#include <vector>
+#include <map>
 
 // The lexer returns tokens [0-255] if it is an unknown character, otherwise one
 // of these for known things.
@@ -17,7 +19,6 @@ enum Token {
 	// primary
 	tok_identifier = -4,
 	tok_num = -5,
-	tok_special = -6,
 };
 
 static std::string g_line;				// Full line of standard input
@@ -27,10 +28,13 @@ bool g_seen_errors = false;				// Whether any errors were encountered while pars
 
 static std::string g_identifier_str;	// Filled in for tok_identifier
 static std::string g_number_str;		// Filled in for tok_num
-static uint8_t g_special_char;			// Filled in if tok_special
 FILE* g_file;							// Global file
 
 
+
+//=================================================================================
+// Lexer
+//=================================================================================
 
 // read_line()
 //	Reads a line from a file into g_line 
@@ -145,27 +149,421 @@ static int get_tok() {
 		}
 
 		//division operator
-		g_special_char = '/';
 		++g_line_idx;
-		return tok_special;
+		return '/';
 	}
 
-	if (isspecial(g_line[g_line_idx])) {
-		g_special_char = g_line[g_line_idx];
-		++g_line_idx;
-		return tok_special;
-	}
-
-	//unknown character
-	fprintf(stderr, "[%lu, %lu]: SyntaxError: Unknown symbol \"%c\".\n", g_line_count, g_line_idx, g_line[g_line_idx]);
-	g_seen_errors = true;
+	//special or unknown character, such as a binary operator or paretheses
 	++g_line_idx;
 	return g_line[g_line_idx - 1];
 }
 
 
 
+//=================================================================================
+// ABSTRACT SYNTAX TREE (AST)
+//=================================================================================
 
+// ExprAST - Base class for all expression nodes.
+class ExprAST {
+	std::string type;
+
+public:
+	ExprAST(const std::string& type) : type(type) {}
+
+	const std::string get_type() const {
+		return type;
+	}
+
+	virtual ~ExprAST() = default;
+};
+
+// DoubleAST - Expression class for floating point numbers, at double precision.
+class DoubleAST : public ExprAST {
+	double val;
+
+public:
+	DoubleAST(const std::string& type, double val) : ExprAST(type), val(val) {}
+};
+
+// IntegerAST - Expression class for integers, taking four bytes of memory.
+class IntegerAST : public ExprAST {
+	int32_t val;
+
+public:
+	IntegerAST(const std::string& type, int32_t val) : ExprAST(type), val(val) {}
+};
+
+// InfIntegerAST - Expression class for integers, with infinite precision.
+class InfIntegerAST : public ExprAST {
+	//this needs to be fixed in the future
+	std::string val;
+
+public:
+	InfIntegerAST(const std::string& type, const std::string& val) : ExprAST(type), val(val) {}
+};
+
+/// VariableExprAST - Expression class for referencing a variable, like "a".
+class VariableExprAST : public ExprAST {
+	std::string name;
+
+public:
+	VariableExprAST(const std::string& type, const std::string& name) : ExprAST(type), name(name) {}
+};
+
+/// BinaryExprAST - Expression class for a binary operator.
+class BinaryExprAST : public ExprAST {
+	char op;
+	std::unique_ptr<ExprAST> lhs, rhs;
+
+public:
+	BinaryExprAST(const std::string& type, char op, std::unique_ptr<ExprAST> lhs, std::unique_ptr<ExprAST> rhs)
+		: ExprAST(type), op(op), lhs(std::move(lhs)), rhs(std::move(rhs)) {
+	}
+};
+
+/// CallExprAST - Expression class for function calls.
+class CallExprAST : public ExprAST {
+	std::string callee;
+	std::vector<std::unique_ptr<ExprAST>> args;
+
+public:
+	CallExprAST(const std::string& type, const std::string& callee, std::vector<std::unique_ptr<ExprAST>> args)
+		: ExprAST(type), callee(callee), args(std::move(args)) {
+	}
+};
+
+// PrototypeAST - This class represents the "prototype" for a function,
+// which captures its name, type, and argument names (thus implicitly the number
+// of arguments the function takes).
+class PrototypeAST : public ExprAST {
+	std::string name;
+	std::vector<std::string> args;
+
+public:
+	PrototypeAST(const std::string& type, const std::string& name, std::vector<std::string> args)
+		: ExprAST(type), name(name), args(std::move(args)) {
+	}
+
+	const std::string& getName() const { return name; }
+};
+
+// FunctionAST - This class represents a function definition itself.
+class FunctionAST {
+	std::unique_ptr<PrototypeAST> proto;
+	std::unique_ptr<ExprAST> body;
+
+public:
+	FunctionAST(std::unique_ptr<PrototypeAST> proto, std::unique_ptr<ExprAST> body)
+		: proto(std::move(proto)), body(std::move(body)) {
+	}
+};
+
+
+//=================
+// Parsing the AST
+//=================
+
+static int g_cur_tok; //current token
+static std::unique_ptr<ExprAST> parse_expression();
+static std::unique_ptr<ExprAST> parse_binop_rhs(int expr_prec, std::unique_ptr<ExprAST> lhs);
+
+
+// get_next_tok()
+//	Update the current token g_cur_tok using the get_tok() function
+static int get_next_tok() {
+	return g_cur_tok = get_tok();
+}
+
+// log_error()
+//	Helper function for error handling. Returns nullptr.
+std::unique_ptr<ExprAST> log_error(const char* str) {
+	fprintf(stderr, "Error: %s\n", str);
+	return nullptr;
+}
+
+// log_error_p()
+//	Helper function for error handling. Returns nullptr.
+std::unique_ptr<PrototypeAST> log_error_p(const char* str) {
+	log_error(str);
+	return nullptr;
+}
+
+
+// parse_double_expr()
+//	Parse a double precision float number expression
+static std::unique_ptr<ExprAST> parse_double_expr() {
+	double d = strtod(g_number_str.c_str(), 0);
+	auto result = std::make_unique<DoubleAST>("double", d);
+	get_next_tok(); // consume the number
+	return std::move(result);
+}
+
+// parse_paren_expr()
+//	Parse a parenthetical expression
+static std::unique_ptr<ExprAST> parse_paren_expr() {
+	// parenexpr := '(' expression ')'
+	get_next_tok(); // eat (.
+
+	//handle whatever expression is within the parentheses
+	auto v = parse_expression();
+	if (!v) {
+		return nullptr;
+	}
+
+	//should be impossible to reach
+	if (g_cur_tok != ')')
+		return log_error("expected ')'");
+
+	get_next_tok(); // eat ).
+
+	return v;
+}
+
+// parse_identifier_expr()
+//	Parse an identifier expression
+static std::unique_ptr<ExprAST> parse_identifier_expr() {
+	// identifierexpr
+	//   := identifier
+	//   := identifier '(' expression* ')'
+
+	std::string id_name = g_identifier_str;
+
+	get_next_tok();  //eat identifier.
+
+	if (g_cur_tok != '(') {
+		return std::make_unique<VariableExprAST>("TODO", id_name);
+	}
+
+	get_next_tok();  // eat (
+	std::vector<std::unique_ptr<ExprAST>> args;
+	if (g_cur_tok != ')') {
+		while (true) {
+			std::unique_ptr<ExprAST> arg = parse_expression();
+			if (arg) {
+				args.push_back(std::move(arg));
+			}
+			else {
+				return nullptr;
+			}
+
+			if (g_cur_tok == ')')
+				break;
+
+			if (g_cur_tok != ',')
+				return log_error("Expected ')' or ',' in argument list");
+			
+			get_next_tok();
+		}
+	}
+
+	// Eat the ')'.
+	get_next_tok();
+
+	return std::make_unique<CallExprAST>("none", id_name, std::move(args));
+}
+
+// parse_primary()
+//	Determines the type of expression to parse and calls the appropriate handler
+static std::unique_ptr<ExprAST> parse_primary() {
+	// primary
+	//   ::= identifierexpr
+	//   ::= numberexpr
+	//   ::= parenexpr
+	switch (g_cur_tok) {
+	case tok_identifier:
+		return parse_identifier_expr();
+	case tok_num:
+		//TODO
+		//THIS ONLY WORKS FOR DOUBLES CURRENTLY!!!
+		return parse_double_expr();
+	case '(':
+		return parse_paren_expr();
+	default:
+		return log_error("unknown token when expecting an expression");
+	}
+}
+
+//mapping from binary operator to precedence value
+static std::map<char, int> binop_precedence;
+
+// set_binop_precedence()
+//	Sets the precedence of all binary operators, such as +, -, *, and /.
+static void set_binop_precedence() {
+	//higher precedence is performed first
+	binop_precedence['<'] = 10;
+	binop_precedence['>'] = 10;
+	binop_precedence['+'] = 20;
+	binop_precedence['-'] = 20;
+	binop_precedence['*'] = 40;
+	binop_precedence['/'] = 40;
+}
+
+
+// get_tok_precedence() 
+//	Get the precedence of the pending binary operator token.
+static int get_tok_precedence() {
+	if (!isascii(g_cur_tok)) {
+		return -1;
+	}
+
+	if (binop_precedence.empty()) {
+		set_binop_precedence();
+	}
+
+	int tok_prec = binop_precedence[g_cur_tok];
+	if (tok_prec <= 0) {
+		//undefined operator
+		return -1;
+	}
+	return tok_prec;
+}
+
+// parse_expression()
+//	Parses an expression into a left-hand-side and a right-hand-side
+static std::unique_ptr<ExprAST> parse_expression() {
+	// expression
+	//   := primary binop_rhs
+	auto lhs = parse_primary();
+	if (!lhs)
+		return nullptr;
+
+	return parse_binop_rhs(0, std::move(lhs));
+}
+
+// parse_binop_rhs(expr_prec, lhs)
+//	Parses the right-hand-side of a binary expression using precedence.
+//	This is functionally an extremely important part of the parser.
+static std::unique_ptr<ExprAST> parse_binop_rhs(int expr_prec, std::unique_ptr<ExprAST> lhs) {
+	// binop_rhs
+	//   := ('+' primary)*
+	while (true) {
+		int tok_prec = get_tok_precedence();
+
+		//if this is a binop that binds at least as tightly as the current binop,
+		//consume it, otherwise we are done.
+		if (tok_prec < expr_prec)
+			return lhs;
+
+		int binop = g_cur_tok;
+		get_next_tok();  // eat binop
+		auto rhs = parse_primary();
+		if (!rhs)
+			return nullptr;
+
+		//if binop binds less tightly with rhs than the operator after rhs, let
+		//the pending operator take rhs as its lhs.
+		int next_prec = get_tok_precedence();
+		if (tok_prec < next_prec) {
+			//use tok_prec + 1 because we know that the rhs must be evaluated with 
+			//minimally higher precedence than the current binop.
+			rhs = parse_binop_rhs(tok_prec + 1, std::move(rhs));
+			if (!rhs)
+				return nullptr;
+		}
+
+		//merge lhs and rhs.
+		lhs = std::make_unique<BinaryExprAST>(lhs->get_type(), binop, std::move(lhs), std::move(rhs));
+	}
+}
+
+// parse_prototype
+//	Parse a function prototype, i.e. its name and arguments
+static std::unique_ptr<PrototypeAST> parse_prototype() {
+	// prototype
+	//   := id '(' id* ')'
+	if (g_cur_tok != tok_identifier)
+		return log_error_p("Expected function name in prototype");
+
+	std::string fn_name = g_identifier_str;
+	get_next_tok();
+
+	if (g_cur_tok != '(')
+		return log_error_p("Expected '(' in prototype");
+
+	//read the list of argument names.
+	std::vector<std::string> arg_names;
+	while (get_next_tok() == tok_identifier)
+		arg_names.push_back(g_identifier_str);
+	if (g_cur_tok != ')')
+		return log_error_p("Expected ')' in prototype");
+
+	//success.
+	get_next_tok();  // eat ')'.
+
+	return std::make_unique<PrototypeAST>("TODO", fn_name, std::move(arg_names));
+}
+
+// parse_function()
+//	Parses a function by getting its protoype and its body expression
+static std::unique_ptr<FunctionAST> parse_function() {
+	// definition := 'def' prototype expression
+	get_next_tok();  // eat def.
+	auto proto = parse_prototype();
+	if (!proto) {
+		return nullptr;
+	}
+
+	auto expr = parse_expression();
+	if (expr) {
+		return std::make_unique<FunctionAST>(std::move(proto), std::move(expr));
+	}
+	return nullptr;
+}
+
+// parse_extern()
+//	Parses an external import.
+static std::unique_ptr<PrototypeAST> parse_extern() {
+	// external ::= 'extern' prototype
+	get_next_tok();  // eat extern.
+	return parse_prototype();
+}
+
+
+//=================================================================================
+// Top level parsing
+//=================================================================================
+
+static void handle_function() {
+	auto fn_ptr = parse_function();
+	if (!fn_ptr) {
+		//skip token for error recovery
+		fprintf(stderr, "[%lu, %lu]: SyntaxError: Attempted and failed to parse function.\n", g_line_count, g_line_idx);
+		get_next_tok();
+	}
+}
+
+static void handle_extern() {
+	auto extern_ptr = parse_extern();
+	if (!extern_ptr) {
+		//skip token for error recovery
+		fprintf(stderr, "[%lu, %lu]: SyntaxError: Attempted and failed to parse an external function.\n", g_line_count, g_line_idx);
+		get_next_tok();
+	}
+}
+
+/// top ::= definition | external | expression | ';'
+static void main_loop() {
+	while (true) {
+		switch (g_cur_tok) {
+		case tok_eof:
+			return;
+		case ';': // ignore top-level semicolons.
+			get_next_tok();
+			break;
+		case tok_def:
+			handle_function();
+			break;
+		case tok_extern:
+			handle_extern();
+			break;
+		default:
+			fprintf(stderr, "[%lu, %lu]: SyntaxError: Unexpected symbol.\n", g_line_count, g_line_idx);
+			get_next_tok();
+			break;
+		}
+	}
+}
 
 
 int main(int argc, char** argv) {
@@ -194,10 +592,16 @@ int main(int argc, char** argv) {
 		return 1;
 	}
 
-	int x = get_tok();
+	//test lexer
+	/*int x = get_tok();
 	while (x != tok_eof) {
-		std::cout << x << ", " << g_identifier_str << ", " << g_number_str << ", " << g_special_char << ", " << std::endl;
+		std::cout << x << ", " << g_identifier_str << ", " << g_number_str << ", " << std::endl;
 		x = get_tok();
-	}
+	}*/
+
+	//test parser
+	main_loop();
+
+
 	return 0;
 }
