a56f4a0891b3cb423db71af6af8452fca83a5247
David Dye <ddye@college.harvard.edu>
2025-03-12 13:48:15 -0400

    Refactoring and adding a requirement for LLVM support.

--- Ankh/lexer.cpp
+++ Ankh/lexer.cpp
@@ -6,6 +6,19 @@
 #include <cstdio>
 #include <vector>
 #include <map>
+#include "llvm/ADT/APFloat.h"
+#include "llvm/ADT/STLExtras.h"
+#include "llvm/IR/BasicBlock.h"
+#include "llvm/IR/Constants.h"
+#include "llvm/IR/DerivedTypes.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/LLVMContext.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IR/Type.h"
+#include "llvm/IR/Verifier.h"
+
+using namespace llvm;
 
 // The lexer returns tokens [0-255] if it is an unknown character, otherwise one
 // of these for known things.
@@ -28,13 +41,131 @@ bool g_seen_errors = false;				// Whether any errors were encountered while pars
 
 static std::string g_identifier_str;	// Filled in for tok_identifier
 static std::string g_number_str;		// Filled in for tok_num
+
 FILE* g_file;							// Global file
 
 
+//======================================================================================================
+// Abstract Syntax Tree (AST)
+//======================================================================================================
+namespace AST {
+	// ExprAST - Base class for all expression nodes.
+	class ExprAST {
+		std::string type;
 
-//=================================================================================
+	public:
+		ExprAST(const std::string& type) : type(type) {}
+
+		const std::string get_type() const {
+			return type;
+		}
+
+		virtual ~ExprAST() = default;
+	};
+
+	// DoubleAST - Expression class for floating point numbers, at double precision.
+	class DoubleAST : public ExprAST {
+		double val;
+
+	public:
+		DoubleAST(const std::string& type, double val) : ExprAST(type), val(val) {}
+	};
+
+	// IntegerAST - Expression class for integers, taking four bytes of memory.
+	class IntegerAST : public ExprAST {
+		int32_t val;
+
+	public:
+		IntegerAST(const std::string& type, int32_t val) : ExprAST(type), val(val) {}
+	};
+
+	// InfIntegerAST - Expression class for integers, with infinite precision.
+	class InfIntegerAST : public ExprAST {
+		//this needs to be fixed in the future
+		std::string val;
+
+	public:
+		InfIntegerAST(const std::string& type, const std::string& val) : ExprAST(type), val(val) {}
+	};
+
+	/// VariableExprAST - Expression class for referencing a variable, like "a".
+	class VariableExprAST : public ExprAST {
+		std::string name;
+
+	public:
+		VariableExprAST(const std::string& type, const std::string& name) : ExprAST(type), name(name) {}
+	};
+
+	/// BinaryExprAST - Expression class for a binary operator.
+	class BinaryExprAST : public ExprAST {
+		char op;
+		std::unique_ptr<ExprAST> lhs, rhs;
+
+	public:
+		BinaryExprAST(const std::string& type, char op, std::unique_ptr<ExprAST> lhs, std::unique_ptr<ExprAST> rhs)
+			: ExprAST(type), op(op), lhs(std::move(lhs)), rhs(std::move(rhs)) {
+		}
+	};
+
+	/// CallExprAST - Expression class for function calls.
+	class CallExprAST : public ExprAST {
+		std::string callee;
+		std::vector<std::unique_ptr<ExprAST>> args;
+
+	public:
+		CallExprAST(const std::string& type, const std::string& callee, std::vector<std::unique_ptr<ExprAST>> args)
+			: ExprAST(type), callee(callee), args(std::move(args)) {
+		}
+	};
+
+	// PrototypeAST - This class represents the "prototype" for a function,
+	// which captures its name, type, and argument names (thus implicitly the number
+	// of arguments the function takes).
+	class PrototypeAST : public ExprAST {
+		std::string name;
+		std::vector<std::string> args;
+
+	public:
+		PrototypeAST(const std::string& type, const std::string& name, std::vector<std::string> args)
+			: ExprAST(type), name(name), args(std::move(args)) {
+		}
+
+		const std::string& getName() const { return name; }
+	};
+
+	// FunctionAST - This class represents a function definition itself.
+	class FunctionAST {
+		std::unique_ptr<PrototypeAST> proto;
+		std::unique_ptr<ExprAST> body;
+
+	public:
+		FunctionAST(std::unique_ptr<PrototypeAST> proto, std::unique_ptr<ExprAST> body)
+			: proto(std::move(proto)), body(std::move(body)) {
+		}
+	};
+
+}
+
+
+// log_error()
+//	Helper function for error handling. Returns nullptr.
+std::unique_ptr<AST::ExprAST> log_error(const char* str) {
+	g_seen_errors = true;
+	fprintf(stderr, "[%lu, %lu]: SyntaxError: %s\n", g_line_count, g_line_idx, str);
+	return nullptr;
+}
+
+// log_error_p()
+//	Helper function for error handling. Returns nullptr.
+std::unique_ptr<AST::PrototypeAST> log_error_p(const char* str) {
+	log_error(str);
+	return nullptr;
+}
+
+
+//======================================================================================================
 // Lexer
-//=================================================================================
+//======================================================================================================
 
 // read_line()
 //	Reads a line from a file into g_line 
@@ -48,19 +179,6 @@ static void read_line() {
 		++g_line_idx;
 	}
 	g_line_idx = 0;
-}
-
-
-// isspecial(c)
-//	Checks whether c is a special character
-static bool isspecial(char c) {
-	const char* specials = "+-/*=()!%^&[]{}|\'\";:,.<>?";
-	for (int i = 0; i < strlen(specials); ++i) {
-		if (c == specials[i]) {
-			return true;
-		}
-	}
-	return false;
 }
 
 
@@ -122,8 +240,7 @@ static int get_tok() {
 		while (isdigit(g_line[g_line_idx]) || g_line[g_line_idx] == '.') {
 
 			if (g_line[g_line_idx] == '.' && seen_period) {
-				fprintf(stderr, "[%lu, %lu]: SyntaxError: Invalid number of decimals in number.\n", g_line_count, g_line_idx);
-				g_seen_errors = true;
+				log_error("Invalid number of decimals in number.");
 				read_line();
 				return get_tok();
 			}
@@ -160,113 +277,13 @@ static int get_tok() {
 
 
 
-//=================================================================================
-// ABSTRACT SYNTAX TREE (AST)
-//=================================================================================
-
-// ExprAST - Base class for all expression nodes.
-class ExprAST {
-	std::string type;
-
-public:
-	ExprAST(const std::string& type) : type(type) {}
-
-	const std::string get_type() const {
-		return type;
-	}
-
-	virtual ~ExprAST() = default;
-};
-
-// DoubleAST - Expression class for floating point numbers, at double precision.
-class DoubleAST : public ExprAST {
-	double val;
-
-public:
-	DoubleAST(const std::string& type, double val) : ExprAST(type), val(val) {}
-};
-
-// IntegerAST - Expression class for integers, taking four bytes of memory.
-class IntegerAST : public ExprAST {
-	int32_t val;
-
-public:
-	IntegerAST(const std::string& type, int32_t val) : ExprAST(type), val(val) {}
-};
-
-// InfIntegerAST - Expression class for integers, with infinite precision.
-class InfIntegerAST : public ExprAST {
-	//this needs to be fixed in the future
-	std::string val;
-
-public:
-	InfIntegerAST(const std::string& type, const std::string& val) : ExprAST(type), val(val) {}
-};
-
-/// VariableExprAST - Expression class for referencing a variable, like "a".
-class VariableExprAST : public ExprAST {
-	std::string name;
-
-public:
-	VariableExprAST(const std::string& type, const std::string& name) : ExprAST(type), name(name) {}
-};
-
-/// BinaryExprAST - Expression class for a binary operator.
-class BinaryExprAST : public ExprAST {
-	char op;
-	std::unique_ptr<ExprAST> lhs, rhs;
-
-public:
-	BinaryExprAST(const std::string& type, char op, std::unique_ptr<ExprAST> lhs, std::unique_ptr<ExprAST> rhs)
-		: ExprAST(type), op(op), lhs(std::move(lhs)), rhs(std::move(rhs)) {
-	}
-};
-
-/// CallExprAST - Expression class for function calls.
-class CallExprAST : public ExprAST {
-	std::string callee;
-	std::vector<std::unique_ptr<ExprAST>> args;
-
-public:
-	CallExprAST(const std::string& type, const std::string& callee, std::vector<std::unique_ptr<ExprAST>> args)
-		: ExprAST(type), callee(callee), args(std::move(args)) {
-	}
-};
-
-// PrototypeAST - This class represents the "prototype" for a function,
-// which captures its name, type, and argument names (thus implicitly the number
-// of arguments the function takes).
-class PrototypeAST : public ExprAST {
-	std::string name;
-	std::vector<std::string> args;
-
-public:
-	PrototypeAST(const std::string& type, const std::string& name, std::vector<std::string> args)
-		: ExprAST(type), name(name), args(std::move(args)) {
-	}
-
-	const std::string& getName() const { return name; }
-};
-
-// FunctionAST - This class represents a function definition itself.
-class FunctionAST {
-	std::unique_ptr<PrototypeAST> proto;
-	std::unique_ptr<ExprAST> body;
-
-public:
-	FunctionAST(std::unique_ptr<PrototypeAST> proto, std::unique_ptr<ExprAST> body)
-		: proto(std::move(proto)), body(std::move(body)) {
-	}
-};
-
-
-//=================
+//======================================================================================================
 // Parsing the AST
-//=================
+//======================================================================================================
 
 static int g_cur_tok; //current token
-static std::unique_ptr<ExprAST> parse_expression();
-static std::unique_ptr<ExprAST> parse_binop_rhs(int expr_prec, std::unique_ptr<ExprAST> lhs);
+static std::unique_ptr<AST::ExprAST> parse_expression();
+static std::unique_ptr<AST::ExprAST> parse_binop_rhs(int expr_prec, std::unique_ptr<AST::ExprAST> lhs);
 
 
 // get_next_tok()
@@ -275,33 +292,19 @@ static int get_next_tok() {
 	return g_cur_tok = get_tok();
 }
 
-// log_error()
-//	Helper function for error handling. Returns nullptr.
-std::unique_ptr<ExprAST> log_error(const char* str) {
-	fprintf(stderr, "Error: %s\n", str);
-	return nullptr;
-}
-
-// log_error_p()
-//	Helper function for error handling. Returns nullptr.
-std::unique_ptr<PrototypeAST> log_error_p(const char* str) {
-	log_error(str);
-	return nullptr;
-}
-
 
 // parse_double_expr()
 //	Parse a double precision float number expression
-static std::unique_ptr<ExprAST> parse_double_expr() {
+static std::unique_ptr<AST::ExprAST> parse_double_expr() {
 	double d = strtod(g_number_str.c_str(), 0);
-	auto result = std::make_unique<DoubleAST>("double", d);
+	auto result = std::make_unique<AST::DoubleAST>("double", d);
 	get_next_tok(); // consume the number
 	return std::move(result);
 }
 
 // parse_paren_expr()
 //	Parse a parenthetical expression
-static std::unique_ptr<ExprAST> parse_paren_expr() {
+static std::unique_ptr<AST::ExprAST> parse_paren_expr() {
 	// parenexpr := '(' expression ')'
 	get_next_tok(); // eat (.
 
@@ -322,7 +325,7 @@ static std::unique_ptr<ExprAST> parse_paren_expr() {
 
 // parse_identifier_expr()
 //	Parse an identifier expression
-static std::unique_ptr<ExprAST> parse_identifier_expr() {
+static std::unique_ptr<AST::ExprAST> parse_identifier_expr() {
 	// identifierexpr
 	//   := identifier
 	//   := identifier '(' expression* ')'
@@ -332,14 +335,14 @@ static std::unique_ptr<ExprAST> parse_identifier_expr() {
 	get_next_tok();  //eat identifier.
 
 	if (g_cur_tok != '(') {
-		return std::make_unique<VariableExprAST>("TODO", id_name);
+		return std::make_unique<AST::VariableExprAST>("TODO", id_name);
 	}
 
 	get_next_tok();  // eat (
-	std::vector<std::unique_ptr<ExprAST>> args;
+	std::vector<std::unique_ptr<AST::ExprAST>> args;
 	if (g_cur_tok != ')') {
 		while (true) {
-			std::unique_ptr<ExprAST> arg = parse_expression();
+			std::unique_ptr<AST::ExprAST> arg = parse_expression();
 			if (arg) {
 				args.push_back(std::move(arg));
 			}
@@ -360,12 +363,12 @@ static std::unique_ptr<ExprAST> parse_identifier_expr() {
 	// Eat the ')'.
 	get_next_tok();
 
-	return std::make_unique<CallExprAST>("none", id_name, std::move(args));
+	return std::make_unique<AST::CallExprAST>("none", id_name, std::move(args));
 }
 
 // parse_primary()
 //	Determines the type of expression to parse and calls the appropriate handler
-static std::unique_ptr<ExprAST> parse_primary() {
+static std::unique_ptr<AST::ExprAST> parse_primary() {
 	// primary
 	//   ::= identifierexpr
 	//   ::= numberexpr
@@ -421,7 +424,7 @@ static int get_tok_precedence() {
 
 // parse_expression()
 //	Parses an expression into a left-hand-side and a right-hand-side
-static std::unique_ptr<ExprAST> parse_expression() {
+static std::unique_ptr<AST::ExprAST> parse_expression() {
 	// expression
 	//   := primary binop_rhs
 	auto lhs = parse_primary();
@@ -434,7 +437,7 @@ static std::unique_ptr<ExprAST> parse_expression() {
 // parse_binop_rhs(expr_prec, lhs)
 //	Parses the right-hand-side of a binary expression using precedence.
 //	This is functionally an extremely important part of the parser.
-static std::unique_ptr<ExprAST> parse_binop_rhs(int expr_prec, std::unique_ptr<ExprAST> lhs) {
+static std::unique_ptr<AST::ExprAST> parse_binop_rhs(int expr_prec, std::unique_ptr<AST::ExprAST> lhs) {
 	// binop_rhs
 	//   := ('+' primary)*
 	while (true) {
@@ -463,13 +466,13 @@ static std::unique_ptr<ExprAST> parse_binop_rhs(int expr_prec, std::unique_ptr<E
 		}
 
 		//merge lhs and rhs.
-		lhs = std::make_unique<BinaryExprAST>(lhs->get_type(), binop, std::move(lhs), std::move(rhs));
+		lhs = std::make_unique<AST::BinaryExprAST>(lhs->get_type(), binop, std::move(lhs), std::move(rhs));
 	}
 }
 
 // parse_prototype
 //	Parse a function prototype, i.e. its name and arguments
-static std::unique_ptr<PrototypeAST> parse_prototype() {
+static std::unique_ptr<AST::PrototypeAST> parse_prototype() {
 	// prototype
 	//   := id '(' id* ')'
 	if (g_cur_tok != tok_identifier)
@@ -491,12 +494,12 @@ static std::unique_ptr<PrototypeAST> parse_prototype() {
 	//success.
 	get_next_tok();  // eat ')'.
 
-	return std::make_unique<PrototypeAST>("TODO", fn_name, std::move(arg_names));
+	return std::make_unique<AST::PrototypeAST>("TODO", fn_name, std::move(arg_names));
 }
 
 // parse_function()
 //	Parses a function by getting its protoype and its body expression
-static std::unique_ptr<FunctionAST> parse_function() {
+static std::unique_ptr<AST::FunctionAST> parse_function() {
 	// definition := 'def' prototype expression
 	get_next_tok();  // eat def.
 	auto proto = parse_prototype();
@@ -506,23 +509,23 @@ static std::unique_ptr<FunctionAST> parse_function() {
 
 	auto expr = parse_expression();
 	if (expr) {
-		return std::make_unique<FunctionAST>(std::move(proto), std::move(expr));
+		return std::make_unique<AST::FunctionAST>(std::move(proto), std::move(expr));
 	}
 	return nullptr;
 }
 
 // parse_extern()
 //	Parses an external import.
-static std::unique_ptr<PrototypeAST> parse_extern() {
+static std::unique_ptr<AST::PrototypeAST> parse_extern() {
 	// external ::= 'extern' prototype
 	get_next_tok();  // eat extern.
 	return parse_prototype();
 }
 
 
-//=================================================================================
+//======================================================================================================
 // Top level parsing
-//=================================================================================
+//======================================================================================================
 
 static void handle_function() {
 	auto fn_ptr = parse_function();
@@ -585,7 +588,8 @@ int main(int argc, char** argv) {
 		return 1;
 	}
 
-	g_file = fopen(filename, "r");	 // Open file in read mode
+	//can safely open the file read-only
+	g_file = fopen(filename, "r");
 
 	if (!g_file) {
 		fprintf(stderr, "Error: Could not open file %s.\n", filename);
@@ -602,6 +606,10 @@ int main(int argc, char** argv) {
 	//test parser
 	main_loop();
 
+	if (g_seen_errors) {
+		fprintf(stderr, "\nParsing failed due to listed errors.\n\n");
+		return 1;
+	}
 
 	return 0;
 }
