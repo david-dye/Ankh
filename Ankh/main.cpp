c9ed1e7e7c89ee3ba120967359a6798b1ebb5ec2
David Dye <ddye@college.harvard.edu>
2025-04-24 01:05:04 -0400

    Added modulus operator and created mod_exp.ank

--- Ankh/main.cpp
+++ Ankh/main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 
 extern "C" {
-    int foo();
+    int mod_exp(int a, int b, int n);
 }
 
 int main() {
-    std::cout << "foo(): " << foo() << std::endl;
+    int a = 649;
+    int b = 550;
+    int n = 37;
+    std::cout << "mod_exp(a, b, c): " << mod_exp(a, b, n) << std::endl;
 }
 
